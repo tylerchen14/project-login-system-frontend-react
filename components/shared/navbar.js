1283a2c8077080d672c42dab4d12c0d1bb91ced3
Tyler <tylerchenpo@gmail.com>
2024-03-25 23:00:39 +0800

    change

--- components/shared/navbar.js
+++ components/shared/navbar.js
@@ -7,13 +7,20 @@ export default function Navbar() {
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
           <Link className="navbar-brand" href="/">首頁</Link>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <button 
+          className="navbar-toggler" 
+          type="button" 
+          data-bs-toggle="collapse" 
+          data-bs-target="#navbarSupportedContent" 
+          aria-controls="navbarSupportedContent" 
+          aria-expanded="false" 
+          aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" href="#">會員名單</Link>
+                <Link className="nav-link active" aria-current="page" href="/memberList">會員名單</Link>
               </li>
               <li className="nav-item">
                 <Link className="nav-link" href="#">Link</Link>
